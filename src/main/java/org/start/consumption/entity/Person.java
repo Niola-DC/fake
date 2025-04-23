355e93d9a02508cd15bfada3917d088de340edc8
GITHUB-USERNAME <itzdonnacruz00@gmail.com>
2025-04-23 08:00:53 -0500

    commit

--- src/main/java/org/start/consumption/entity/Person.java
+++ src/main/java/org/start/consumption/entity/Person.java
@@ -11,11 +11,6 @@ public class Person {
     String name;
     String email;
 
-    public Person(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
     public String getName() {
         return name;
     }
